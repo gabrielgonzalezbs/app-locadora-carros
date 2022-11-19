99f49f8bc43697eff6ece93dcb30d1f183a2eb0b
gabrielgonzalez <gabriel.gonzalezbs@gmail.com>
2022-11-19 00:42:52 -0300

    FEAT: Criação de componente INPUT
    Realizada criação do componente de inputs do tipo texto

---
+++ resources/js/components/forms/InputComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="form-group">
+        <label :for="idInput" class="form-label">{{textLabel}}</label>
+        <input
+            :type="typeInput"
+            class="form-control"
+            :id="idInput"
+            :value="value"
+            @input="$emit('input', $event.target.value)" >
+
+        <div v-if="textHelp != ''" class="form-text">{{textHelp}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'idInput',
+        'textLabel',
+        'typeInput',
+        'textHelp',
+        'value'
+    ],
+
+}
+</script>
