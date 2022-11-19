5335d8db3117bb9616a8b94ad3a04707501e49e3
gabrielgonzalez <gabriel.gonzalezbs@gmail.com>
2022-11-19 19:26:46 -0300

    FEAT: Criação do componente MODAL
    Craido conponente modal para ciração e manutenção

---
+++ resources/js/components/forms/ModalComponent.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" :data-bs-target="`#${modalId}`">
+            {{buttonText}}
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" :id="modalId" tabindex="-1" :aria-labelledby="`${modalId}Label`" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" :id="`${modalId}Label`">
+                            {{modalTitle}}
+                        </h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+
+                        <slot name="modal-body">
+
+                        </slot>
+
+                    </div>
+                    <div class="modal-footer">
+                        <slot name="modal-footer">
+
+                        </slot>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'modalId',
+        'buttonText',
+        'modalTitle'
+    ]
+}
+</script>
+
+<style>
+
+</style>
