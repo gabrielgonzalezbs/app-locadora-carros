12dae52da7ba7e62df14c0b7eba1974b363be5b4
gabrielgonzalez <gabriel.gonzalezbs@gmail.com>
2022-12-11 17:36:13 -0300

    FEAT: Instalação do VUEX
    Adicionado dependencia do VUEX

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,8 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
+import Vuex from "Vuex";
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
